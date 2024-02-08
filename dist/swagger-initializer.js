ddd6f1b669b51e9d0ca9ffdc059993a2ac6c249d
Diego Herrera <153771070+diego-herrera-softtek@users.noreply.github.com>
2024-02-08 13:55:17 -0300

    Update swagger-initializer.js
    Modificación de referencia de archivo YAML

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "openapi_v1.1.0.yaml",
+    url: "openapi_v1.2.0.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
